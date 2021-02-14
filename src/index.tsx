8f5e60f717722a89a938004d6019d2b5129bacc8
Stéphan Gaud <stephangaud@gmail.com>
2021-02-14 17:56:12 +0000

    add basic CSS styling to editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
